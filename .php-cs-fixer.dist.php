729591d4d1c9807df7f19b0c6c39b582c1cfa386
jvspeed74 <javaugh@iu.edu>
2026-02-14 15:25:30 -0500

    fix: LF line ending

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -16,4 +16,4 @@ return (new PhpCsFixer\Config('CI'))
     ->setRules(['@PER-CS2.0' => true, '@PHP82Migration' => true])
     ->setCacheFile(__DIR__ . '/.php-cs-fixer/.php-cs-fixer.cache')
     ->setFinder($finder)
-    ->setLineEnding("\r\n");
+    ->setLineEnding("\n");
